e285531df41b8fe626470989226d8dc680eea7a4
osorosia <64416398+osorosia@users.noreply.github.com>
2022-03-30 07:11:19 +0900

    single, pageを追加

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -1,6 +1,6 @@
 <article>
     <header>
-        <?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
+        <?php the_title( '<h1 class="single-title">', '</h1>' ); ?>
         <?php the_content(); ?>
     </header>
 </article>
