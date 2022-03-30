2b0cef193758cc4b4bfd7bcfcd00ec48e36f8917
osorosia <64416398+osorosia@users.noreply.github.com>
2022-03-30 10:47:29 +0900

    カテゴリ、タグの表示

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -1,6 +1,20 @@
 <article <?php post_class("single")?> >
-    <header>
+    <header class="single-header">
         <?php the_title( '<h1 class="single-title">', '</h1>' ); ?>
-        <?php the_content(); ?>
     </header>
+
+    <div class="single-content">
+        <?php the_content(); ?>
+    </div>
+    
+    <footer class="single-footer">
+        <div class="single-categories">
+            <p>カテゴリ：<?php the_category(' '); ?></p>
+        </div>
+
+        <div class="single-tags">
+            <p>タグ：<?php the_tags('', ' ', '') ?></p>
+        </div>
+    </footer>
+
 </article>
